25e06fcd78e2e0a55612da59a74bd6f665448e25
lavesh kumbhalkar <lavesh.kumbhalkar@gmail.com>
2023-02-24 00:34:40 +0530

    create new file and write

--- nodeCURD/index.js
+++ nodeCURD/index.js
@@ -2,4 +2,6 @@ const fs = require("fs");
 
 // fs.mkdirSync("testing"); //create new folder
 
-fs.writeFileSync("testing/bio.txt", "this my testing folder");
+// fs.writeFileSync("testing/bio.txt", "this my testing folder"); //create new file with new data
+
+fs.appendFileSync("testing/bio.txt", "/n my new data append on bio file")
