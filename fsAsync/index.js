6d7a476e9206ea3014651eeecf8bc1f7dd607499
lavesh kumbhalkar <lavesh.kumbhalkar@gmail.com>
2023-02-24 01:53:25 +0530

    append data to read file

--- fsAsync/index.js
+++ fsAsync/index.js
@@ -1,6 +1,11 @@
+const { Console } = require('console');
 const fs = require('fs');
 
-fs.writeFile("read.txt", "welcome to my github repository", (err) => {
-    console.log("file crated successfully");
-    console.log(err);
-});
+// fs.writeFile("read.txt", "welcome to my github repository", (err) => {
+//     console.log("file crated successfully");
+//     console.log(err);
+// });
+
+fs.appendFile("read.txt", " please update my profile image", (err) => {
+    console.log("file update successfully");
+})
